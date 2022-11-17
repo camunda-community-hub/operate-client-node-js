c92b0bc86b588df11f90b8430f43af4e7965bb2b
Josh Wulf <josh@magikcraft.io>
2022-11-17 18:22:56 +1300

    Update test

--- src/__test__/test.spec.ts
+++ src/__test__/test.spec.ts
@@ -2,12 +2,11 @@ import { OperateClient } from "../lib/OperateClient";
 
 const c = new OperateClient();
 
-
-(async () => {
+test('It can talk to the API and get a response', async () => {
     const defs = await c.getProcessDefinitions()
     console.log(defs)
     expect(defs).toBeTruthy()
     const p = await c.getProcessDefinition(2251799816930533)
     console.log(p)
     expect(p).toBeTruthy()
-})()
+})
