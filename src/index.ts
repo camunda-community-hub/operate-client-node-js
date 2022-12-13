b9d5e6058923c532924658ccd5abc1e1cf114010
Josh Wulf <josh@magikcraft.io>
2022-12-13 17:57:18 +1300

    update doc

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 export { OperateApiClient } from "./lib/OperateApiClient";
+
+
+export { 
+    ChangeStatus, 
+    FlownodeInstance, 
+    Incident, 
+    ProcessDefinition, 
+    ProcessInstance, 
+    Query, 
+    SearchResults, 
+    Variables 
+} from "./lib/APIObjects"
--- src/lib/OperateApiClient.ts
+++ src/lib/OperateApiClient.ts
@@ -7,14 +7,34 @@ const pkg = require('../../package.json')
 
 const OPERATE_API_VERSION = 'v1'
 
+/**
+ * @description The high-level client for Operate.
+ * @example
+ * ```
+ * const operate = new OperateApiClient()
+ * 
+ * operate.searchProcessInstances({
+ *     filter: {
+ *         state: "ACTIVE"
+ *     },
+ *     size: 50
+ * }).then(instances => {
+ *     console.log(instances)
+ * })
+ * ```
+ */
 export class OperateApiClient {
-    userAgentString: string;
-    baseUrl: string;
-    gotOptions: { prefixUrl: string; };
+    private userAgentString: string;
+    private gotOptions: { prefixUrl: string; };
 
-    constructor(baseUrl: string = 'https://bru-2.operate.camunda.io') {
+    /**
+     * @example
+     * ```
+     * const operate = new OperateApiClient()
+     * ```
+     */
+    constructor() {
         this.userAgentString = `operate-client-nodejs/${pkg.version}`
-        this.baseUrl = baseUrl
         const creds = getOperateCredentials()
         this.gotOptions = {
             prefixUrl: `${creds.CAMUNDA_OPERATE_BASE_URL}/${OPERATE_API_VERSION}`
@@ -30,6 +50,26 @@ export class OperateApiClient {
         }
     }
 
+    /**
+     * @description Search and retrieve process definitions. 
+     * 
+     * [Camunda 8 Documentation](https://docs.camunda.io/docs/apis-clients/operate-api/#process-definition)
+     * @example
+     * ```
+     * const query: Query<ProcessDefinition> = {
+     *   filter: {},
+     *   size: 50,
+     *   sort: [
+     *     {
+     *       field: "bpmnProcessId",
+     *       order: "ASC",
+     *     },
+     *    ],
+     *  };
+     * const operate = newOperateClient()
+     * const defs = await operate.searchProcessDefinitions(query);
+     * ```
+     */
     public async searchProcessDefinitions(query: Query<ProcessDefinition> = {}): Promise<SearchResults<ProcessDefinition>> {
         const headers = await this.getHeaders()
         return got.post(`process-definitions/search`, {
@@ -39,6 +79,17 @@ export class OperateApiClient {
         }).json()
     }
 
+    /**
+     * 
+     * @description Retrieve the metadata for a specific process definition, by key.  
+     * 
+     * [Camunda 8 Documentation](https://docs.camunda.io/docs/apis-clients/operate-api/#process-definition)
+     * @example
+     * ```
+     * const operate = new OperateApiClient()
+     * const definition = await operate.getProcessDefinition(2251799817140074);
+     *  ```
+     */
     public async getProcessDefinition(processDefinitionKey: number): Promise<ProcessDefinition> {
         const headers = await this.getHeaders()
         return got(`process-definitions/${processDefinitionKey}`, {
@@ -55,6 +106,26 @@ export class OperateApiClient {
         }).text()
     }
 
+    /**
+     * @description Search and retrieve process instances.
+     * @example 
+     * ```
+     * const operate = new OperateApiClient()
+     * const query: Query<ProcessInstance>  = {
+     *   filter: {
+     *     processVersion: 1
+     *   },
+     *   size: 50,
+     *   sort: [
+     *     {
+     *       field: "bpmProcessId",
+     *       order: "ASC"
+     *     }
+     *   ]
+     * }
+     * const instances = await operate.searchProcessInstances(query)
+     * console.log(`Found ${instances.total} instances`)
+     */
     public async searchProcessInstances(query: Query<ProcessInstance> = {}): Promise<SearchResults<ProcessInstance>> {
         const headers = await this.getHeaders()
         return got.post(`process-instances/search`, {
@@ -64,6 +135,15 @@ export class OperateApiClient {
         }).json()
     }
 
+    /**
+     * 
+     * @description Retrieve a specific process instance by id. 
+     * @example
+     * ```
+     * const operate = new OperateApiClient()
+     * const instance = await operate.getProcessInstance(2251799819847322)
+     * ```
+     */
     public async getProcessInstance(processInstanceKey: number): Promise<ProcessInstance> {
         const headers = await this.getHeaders()
         return got(`process-instances/${processInstanceKey}`, {
@@ -72,6 +152,14 @@ export class OperateApiClient {
         }).json()
     }
 
+    /**
+     * @description Delete a specific process instance by key.
+     * @example  
+     * ```
+     * const operate = new OperateApiClient()
+     * await operate.deleteProcessInstance(2251799819847322)
+     * ``` 
+     */
     public async deleteProcessInstance(processInstanceKey: number): Promise<ChangeStatus> {
         const headers = await this.getHeaders()
         return got.delete(`process-instances/${processInstanceKey}`, {
@@ -80,6 +168,27 @@ export class OperateApiClient {
         }).json()
     }
 
+    /**
+     * 
+     * @description Search and retrieve incidents. 
+     * @example
+     * ```
+     * const operate = new OperateApiClient()
+     * const query: Query<Incident> = {
+     *   filter: {
+     *     state: "ACTIVE"
+     *   },
+     *   size: 50,
+     *   sort: [
+     *     {
+     *       field: "creationTime",
+     *       order: "ASC"
+     *     }
+     *   ]
+     * }
+     * const incidents = operate.searchIncidents(query)
+     * ``` 
+     */
     public async searchIncidents(query: Query<Incident> = {}): Promise<SearchResults<Incident>> {
         const headers = await this.getHeaders()
         return got.post(`incidents/search`, {
@@ -89,6 +198,16 @@ export class OperateApiClient {
         }).json()
     }
 
+    /**
+     * 
+     * @description Retrieve an incident by incident key. 
+     * @example
+     * ```
+     * const operate = new OperateApiClient()
+     * const incident = await operate.getIncident(2251799818436725)
+     * console.log(incident.message)
+     * ``` 
+     */
     public async getIncident(key: number): Promise<Incident> {
         const headers = await this.getHeaders()
         return got(`incidents/${key}`, {
